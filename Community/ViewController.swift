9fc7bff8f6279eb73c2438066446a89fb02b69b7
georgepazdralwork <georgpaz@aol.com>
2018-10-09 06:07:55 -0500

    Initial Commit

---
+++ Community/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Community
+//
+//  Created by George Pazdral (work) on 10/9/18.
+//  Copyright © 2018 George Pazdral (work). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
